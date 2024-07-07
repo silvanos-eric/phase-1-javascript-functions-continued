5ae095e4a4322158557284173f7e06479129a0d5
Silvanos Eric <ericoderito@gmail.com>
2024-07-07 20:05:24 +0300

    feat(index.js): add default activity functions
    - Implement saturdayFun function with default activity 'roller-skate'
    - Define mondayWork function with default activity 'go to the office'
    - Create wrapAdjective function to wrap a specified adjective with a character

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(paramOuter = "*") {
+  return function (paramInner = "special") {
+    return `You are ${paramOuter}${paramInner}${paramOuter}!`;
+  };
+}
